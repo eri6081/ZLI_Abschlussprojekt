acd752c2a316d3cc5ab6c1f4b777280d9281a724
eri6081 <114018548+eri6081@users.noreply.github.com>
2023-06-26 15:41:53 +0200

    Added a script to handle coin collisions

---
+++ pickCoins.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class pickCoins : MonoBehaviour
+{
+    public int value;
+
+         
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            CoinCounter.instance.IncreaseCoins(value);
+        }
+    }
+}
